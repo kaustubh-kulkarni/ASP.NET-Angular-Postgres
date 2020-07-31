dbb6703dcda53b2b9a912fcc9fef11124e35eb1a
kaustubh__ <kulkarnikaustubhv@gmail.com>
2020-07-31 12:44:43 +0200

    Added pie chart

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -9,6 +9,16 @@ export class PieChartComponent implements OnInit {
 
   constructor() { }
 
+  pieChartData: number [] = [350, 450, 120];
+  pieChartLabels: string [] = ['Mark', 'Bob', 'Tom'];
+  colors: any[] = [
+    {
+      backgroundColor: ['#26547c', '#ff6b8b', '#ffd166'],
+      borderColor: '#111'
+    }
+  ];
+  pieChartType = 'pie';
+
   ngOnInit(): void {
   }
 
