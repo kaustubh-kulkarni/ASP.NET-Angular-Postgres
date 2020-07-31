d9007d4bd5686b5b16fa831cf4e707bff74d727e
kaustubh__ <kulkarnikaustubhv@gmail.com>
2020-07-31 14:21:22 +0200

    Added moch data in order section

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order} from '../../shared/order';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
@@ -8,6 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {   id: 1,
+        customer:{id: 1, name: 'Agilent', state: 'CO', email: 'agilent@gmail.com'},
+        total: 230,
+        placed: new Date(2020, 1, 1),
+        fulfilled: new Date(2020, 5, 1),
+    },
+    {   id: 2,
+      customer:{id: 2, name: 'Philips', state: 'CO', email: 'philips@gmail.com'},
+      total: 45,
+      placed: new Date(2020, 5, 1),
+      fulfilled: new Date(2020, 5, 1),
+  },
+  {   id: 3,
+    customer:{id: 1, name: 'Agilent', state: 'CO', email: 'agilent@gmail.com'},
+    total: 230,
+    placed: new Date(2020, 6, 1),
+    fulfilled: new Date(2020, 5, 1),
+},
+{   id: 4,
+  customer:{id: 1, name: 'Agilent', state: 'CO', email: 'agilent@gmail.com'},
+  total: 230,
+  placed: new Date(2020, 10, 1),
+  fulfilled: new Date(2020, 5, 1),
+},
+{   id: 5,
+  customer:{id: 1, name: 'Agilent', state: 'CO', email: 'agilent@gmail.com'},
+  total: 230,
+  placed: new Date(2020, 11, 1),
+  fulfilled: new Date(2020, 5, 1),
+},
+  ];
 
   ngOnInit(): void {
   }
